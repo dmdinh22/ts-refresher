150924d57a456de0fe50276489ad469e7027e022
David Dinh <dmdinh22@gmail.com>
2019-01-13 23:59:09 -0800

    function with types and return types

--- functions.js
+++ functions.js
@@ -1,4 +1,24 @@
 function getSum(num1, num2) {
     return num1 + num2;
 }
-console.log(getSum(1, 4));
+// console.log(getSum(1, 4));
+var mySum = function (num1, num2) {
+    if (typeof num1 == 'string') {
+        num1 = parseInt(num1);
+    }
+    if (typeof num2 == 'string') {
+        num2 = parseInt(num1);
+    }
+    return num1 + num2;
+};
+// console.log(mySum('3', 5));
+function getName(firstName, lastName) {
+    if (!lastName) {
+        return firstName;
+    }
+    return firstName + " " + lastName;
+}
+console.log(getName('Slim'));
+function voidFunc() {
+    return;
+}
