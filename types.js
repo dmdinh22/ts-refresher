67ad30e9a9ae1e9045caa7027fa23b781ccf70d8
David Dinh <dmdinh22@gmail.com>
2019-01-13 23:01:01 -0800

    ts types

--- types.js
+++ types.js
@@ -1,5 +1,28 @@
 var myString;
 var myNum;
-myString = 'Hello World!';
-myNum = 22;
-console.log(myNum);
+var myBool;
+var something;
+var myVar;
+// one way of defining arrays
+// let strArray: String[];
+// let numArray: Number[];
+// let boolArray: Boolean[];
+// another way of defining arrays
+var strArray;
+var numArray;
+var boolArray;
+// tuples
+var strNumTuple;
+something = 'what';
+myString = something + " is going on";
+myNum = 5.5;
+myBool = false;
+myVar = 'string';
+strArray = ['Hello', 'is', 'it', 'me', 'you', 'are', 'looking', 'for'];
+numArray = [1, 2, 3];
+boolArray = [true, false, true];
+strNumTuple = ['test', 4];
+var myVoid = undefined;
+var myNull = null;
+var myUndefined = undefined;
+console.log(myVoid);
--- types.ts
+++ types.ts
@@ -1,7 +1,36 @@
 let myString: String;
 let myNum: Number;
+let myBool: Boolean;
+let something: String;
+let myVar: any;
 
-myString = 'Hello World!';
-myNum = 22;
+// one way of defining arrays
+// let strArray: String[];
+// let numArray: Number[];
+// let boolArray: Boolean[];
 
-console.log(myNum);
+// another way of defining arrays
+let strArray: Array<String>;
+let numArray: Array<Number>;
+let boolArray: Array<Boolean>;
+
+// tuples
+let strNumTuple: [String, Number];
+
+something = 'what';
+myString = `${something} is going on`;
+myNum = 5.5;
+myBool = false;
+myVar = 'string';
+
+strArray = ['Hello', 'is', 'it', 'me', 'you', 'are', 'looking', 'for'];
+numArray = [1, 2, 3];
+boolArray = [true, false, true];
+
+strNumTuple = ['test', 4];
+
+let myVoid: void = undefined;
+let myNull: null = null;
+let myUndefined: undefined = undefined;
+
+console.log(myVoid);
